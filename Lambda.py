6be40303ecbf32c9856fdc2e7c0721454406bca0
IberthAlexander <minaya.iberth@gmail.com>
2022-02-22 13:25:41 -0500

    Agregar el proyecto2

--- Lambda.py
+++ Lambda.py
@@ -1,8 +1,9 @@
 import json
+import boto
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda2!')
+        'body': json.dumps('Prueba de GitHub Lambda!')
     }
