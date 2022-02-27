3a3116de589f3c3b76cedfc959f79c9ce9a46e1e
IberthAlexander <100225919+IberthAlexander@users.noreply.github.com>
2022-02-26 23:54:34 -0500

    Delete Lambda.py

--- Lambda.py
+++
@@ -1,9 +0,0 @@
-import json
-import boto
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Prueba de GitHub Lambda!')
-    }
